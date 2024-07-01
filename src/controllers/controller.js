d12472cb3a9201f4c2acaee307258cda55a31014
Jorge <jorge.arivilca@gmail.com>
2024-06-30 19:41:37 -0500

    se hicieron las rutas de put y delete, form login, registro, usuarios

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -16,7 +16,7 @@ const listarTodo = async(req, res) => {
 
 /**Ruta de formulario para registrar mascota */
 const registroMascota = (req, res) => {
-    res.render('pages/formRegistroMascota');
+    res.render('pages/registroPerros');
 };
 
 
@@ -25,7 +25,6 @@ const agregarMascota = async(req, res) => {
         console.log("No se ha cargado ninguna imagem")
     }
     const image = url_imagen + req.file.filename;
-    console.log(image)
     const { nombre, raza, sexo, edad, peso, tamanio, fecha_ingreso, adoptado, imagen } = req.body;
     if (nombre === "" || raza ==="" || sexo === "" || edad === "" || peso === "" || tamanio === "" || fecha_ingreso === "" || adoptado === "" || imagen === "") {
         return res.status(400).send('Todos los campos requeridos deben ser completados');
@@ -37,17 +36,94 @@ const agregarMascota = async(req, res) => {
             console.error('Error en la consulta SQL:', err);
             return res.status(500).send('Error al registrar el perro');
         }
-        res.render('pages/registroEnviado');
+        // res.render('pages/registroEnviado');
+        res.status(201).json({msg:'Recurso creado'})
     });
 }
   
 const logIn = (req, res) => {
     res.render('pages/logIn')
+};
+
+const registroUsuario =(req, res) => {
+    res.render('pages/registroUsuario')
+};
+
+const agregarUsuario = (req, res) => {
+    if(!req.file) {
+        console.log("No se ha cargado ninguna imagem")
+    }
+    const image = url_imagen + req.file.filename;
+    const {nombre, direccion, telefono, email, fecha_adopcion, imagen} = req.body;
+    if(nombre === "" || direccion === "" || telefono === "" || email === "" || fecha_adopcion === "" || imagen === "") {
+        return res.status(400).send('Todos los campos requeridos deben ser completados');
+    }
+    const sql = 'INSERT INTO adoptantes (nombre, direccion, telefono, email, fecha_adopcion, imagen) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [nombre, direccion, telefono, email, fecha_adopcion, image]
+    pool.query(sql, values, (err, result) => {
+        if(err) {
+            console.error('Error en la consulta SQL: ', err);
+            return res.status(500).send('Error al registrar el usuario');
+        }
+        res.status(201).json({msg:'Usuario creado'})
+        console.log(result)
+    })
+};
+
+const obtenerAdoptantes = (req, res) =>{
+    const sql = `SELECT * FROM adoptantes`;
+    pool.query(sql, (error, results) => {
+        if(error) {
+            console.log(`Error en la consulta SQL: `, error)
+            return res.status(500).send(`Error al obtener los datos`)
+        } else {
+            res.status(200).json(results)
+        }
+    })
 }
+
+
+const actualizarAdoptante = async (req, res) => {
+    const { id } = req.params;
+    const { nombre, direccion, telefono, email, fecha_adopcion } = req.body;
+
+    if (!nombre || !direccion || !telefono || !email || !fecha_adopcion) {
+        return res.status(400).send('Todos los campos requeridos deben ser completados');
+    }
+
+    try {
+        const sql =  'UPDATE adoptantes SET nombre = ?, direccion = ?, telefono = ?, email = ?, fecha_adopcion = ? WHERE id_adoptantes= ?';
+        const values = [nombre, direccion, telefono, email, fecha_adopcion, id];
+        await pool.query(sql, values);
+        res.status(200).send('Adoptante actualizado exitosamente');
+    } catch (error) {
+        console.error('Error al actualizar adoptante:', error);
+        res.status(500).send('Error al actualizar adoptante');
+    }
+};
+
+
+const eliminarAdoptante = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const sql = 'DELETE FROM adoptantes WHERE id_adoptantes = ?';
+        await pool.query(sql, [id]);
+        res.status(200).send('Adoptante eliminado exitosamente');
+    } catch (error) {
+        console.error('Error al eliminar adoptante:', error);
+        res.status(500).send('Error al eliminar adoptante');
+    }
+};
 
 module.exports = {
     listarTodo,
     registroMascota,
     agregarMascota,
-    logIn
+    logIn,
+    registroUsuario,
+    agregarUsuario,
+    obtenerAdoptantes,
+    actualizarAdoptante,
+    eliminarAdoptante
 }
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,14 +1,24 @@
-const { listarTodo, registroMascota, agregarMascota, logIn } = require('../controllers/controller');
+const { listarTodo, registroMascota, agregarMascota, logIn, registroUsuario, agregarUsuario, actualizarAdoptante, obtenerAdoptantes, eliminarAdoptante } = require('../controllers/controller');
 const express = require('express');
 const router = express.Router();
 const upload = require('../utils/handleStorage');
 
 router.get('/nuestrosPerros', listarTodo);
 
-router.get('/formRegistroMascota', registroMascota);
+router.get('/formularioPerros', registroMascota);
 
 router.post('/registroEnviado', upload.single('imagen'), agregarMascota);
 
 router.get('/logIn', logIn );
+
+router.get('/registroUsuario', registroUsuario);
+
+router.post('/agregarUsuario', upload.single('imagen'), agregarUsuario);
+
+router.get('/adoptantes', obtenerAdoptantes)
+
+router.put('/adoptante/:id', actualizarAdoptante);
+
+router.delete('/adoptante/:id', eliminarAdoptante)
 
 module.exports = router;
